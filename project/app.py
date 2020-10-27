d91bb70e23c5cf7e6c2be80686b93cc8d3629b15
JaneIllario <jane.illarionova@mail.utoronto.ca>
2020-10-27 13:24:15 -0400

    create login/logout route

--- project/app.py
+++ project/app.py
@@ -40,6 +40,29 @@ def close_db(error):
     if hasattr(g, "sqlite_db"):
         g.sqlite_db.close()
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """User login/authentication/session management."""
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    """User logout/authentication/session management."""
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
+
 @app.route("/")
 def index():
     """Searches the database for entries, then displays them."""
