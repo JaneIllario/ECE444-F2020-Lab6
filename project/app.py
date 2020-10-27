7e9185728e5b8f31bd68f9f0d7e37916d0e1d5b7
JaneIllario <jane.illarionova@mail.utoronto.ca>
2020-10-27 13:52:12 -0400

    move templates into project, add route for db write

--- project/app.py
+++ project/app.py
@@ -66,6 +66,20 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('index'))
 
+@app.route('/add', methods=['POST'])
+def add_entry():
+    """Add new post to database."""
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute(
+        'insert into entries (title, text) values (?, ?)',
+        [request.form['title'], request.form['text']]
+    )
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('index'))
+
 @app.route("/")
 def index():
     """Searches the database for entries, then displays them."""
